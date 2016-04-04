650f20142e213d3724ffc6d6f8ec58ab3ba2e1a3
Rencie Bautista <rencie.bautista@yahoo.com>
2016-04-04 14:20:57 +0800

    add fix for inventory

--- database/seeds/FixWrongDivisionTableSeeder.php
+++ database/seeds/FixWrongDivisionTableSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Seeder;
 // composer require laracasts/testdummy
 use Laracasts\TestDummy\Factory as TestDummy;
 use App\Models\ItemInventories;
+use App\Models\AssortmentItemInventories;
 use App\Models\Item;
 
 class FixWrongDivisionTableSeeder extends Seeder
@@ -12,9 +13,20 @@ class FixWrongDivisionTableSeeder extends Seeder
     public function run()
     {
     	$divisions = ['HPC CATEGORY', 'FOODS CATEGORY'];
-        $inventories = ItemInventories::whereNotIn('division',$divisions)->get();
 
+        $inventories = ItemInventories::whereNotIn('division',$divisions)->get();
         foreach ($inventories as $inventory) {
+        	$item = Item::where('sku_code',$inventory->sku_code)->first();
+        	$inventory->division = $item->division->division;
+        	$inventory->category = $item->category->category;
+        	$inventory->category_long = $item->category->category_long;
+        	$inventory->sub_category = $item->subcategory->sub_category;
+        	$inventory->brand = $item->brand->brand;
+        	$inventory->update();
+        }
+
+        $asinventories = AssortmentItemInventories::whereNotIn('division',$divisions)->get();
+        foreach ($asinventories as $inventory) {
         	$item = Item::where('sku_code',$inventory->sku_code)->first();
         	$inventory->division = $item->division->division;
         	$inventory->category = $item->category->category;
