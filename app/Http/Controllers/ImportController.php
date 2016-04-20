c1d843ca19d645823ff28a178a21e5ba169bba6e
Rencie Bautista <rencie.bautista@yahoo.com>
2016-04-20 09:19:35 +0800

    test email

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Session;
-
+use App\Setting;
 use App\Models\UpdateHash;
 use App\Jobs\UpdateMasterfile;
 
