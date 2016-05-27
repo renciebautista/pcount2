51741854ffc2ab027451663e3f8dd5326b092d1c
Rencie Bautista <rencie.bautista@yahoo.com>
2016-05-27 16:16:11 +0800

    add posting history report

--- resources/views/devices/index.blade.php
+++ resources/views/devices/index.blade.php
@@ -16,7 +16,7 @@
 {!! Html::linkRoute('devices.create', 'Export Device List', array(), ['class' => 'btn btn-success btn-sm']) !!}
 
 
-<label class="pull-right">{{ $device->count() }} records found.</label>
+<label class="pull-right">{{ $devices->count() }} records found.</label>
 <table class="table table-striped table-hover">
     <thead>
         <tr>
